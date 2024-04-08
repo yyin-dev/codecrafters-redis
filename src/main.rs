3be47ac334de255c98d00f7ef8a6d326122e049a
Yue Yin <yueyin.dev@gmail.com>
2024-04-07 21:30:44 -0400

    refactor

--- src/main.rs
+++ src/main.rs
@@ -1,122 +1,21 @@
+mod server;
 mod store;
 
-use anyhow::Result;
-use redis::{FromRedisValue, RedisResult, Value};
-use redis_protocol::resp2::{encode::encode, types::OwnedFrame, types::Resp2Frame};
-use std::{
-    io::Write,
-    net::{TcpListener, TcpStream},
-    sync::Arc,
-    thread,
-    time::Duration,
-};
-use store::Store;
+use std::{net::TcpListener, sync::Arc, thread};
 
 fn main() {
-    let store = Arc::new(Store::new());
+    let server = Arc::new(server::Server::new());
 
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
     for stream in listener.incoming() {
         match stream {
             Ok(stream) => {
-                let store = store.clone();
-                thread::spawn(move || handle_client(stream, store).unwrap());
+                let server = server.clone();
+                thread::spawn(move || server.handle_new_client(stream));
             }
             Err(e) => {
                 println!("error: {}", e);
             }
         }
     }
-}
-
-fn write_frame(stream: &mut TcpStream, frame: OwnedFrame) -> Result<()> {
-    let mut buf = vec![0; frame.encode_len()];
-    encode(&mut buf, &frame)?;
-    stream.write_all(&buf)?;
-    Ok(())
-}
-
-fn handle_client(mut stream: TcpStream, store: Arc<Store>) -> Result<()> {
-    println!("Connection established");
-
-    let mut parser = redis::Parser::new();
-    loop {
-        let result = parser.parse_value(&stream);
-
-        match result {
-            Ok(value) => handle_redis_value(&mut stream, store.as_ref(), value)?,
-            Err(error) => {
-                println!("parse error, will drop connection: {:?}", error.category());
-                break;
-            }
-        }
-    }
-
-    Ok(())
-}
-
-fn handle_redis_value(stream: &mut TcpStream, store: &Store, value: Value) -> Result<()> {
-    match value {
-        redis::Value::Nil => todo!(),
-        redis::Value::Int(_) => todo!(),
-        redis::Value::Data(data) => {
-            println!("Data: {:?}", data);
-        }
-        redis::Value::Bulk(values) => {
-            println!("Bulk: {:?}", values);
-
-            let string_from = |idx: usize| -> RedisResult<String> {
-                String::from_owned_redis_value(values.get(idx).unwrap().clone())
-            };
-
-            match &values[0] {
-                redis::Value::Data(command) => {
-                    match String::from_utf8_lossy(command)
-                        .to_ascii_lowercase()
-                        .as_str()
-                    {
-                        "ping" => write_frame(stream, OwnedFrame::BulkString("PONG".into()))?,
-                        "echo" => {
-                            assert_eq!(values.len(), 2);
-                            let string = string_from(1)?;
-                            write_frame(stream, OwnedFrame::BulkString(string.into()))?
-                        }
-                        "get" => {
-                            assert_eq!(values.len(), 2);
-                            let key = string_from(1)?;
-                            match store.get(&key) {
-                                None => write_frame(stream, OwnedFrame::Null)?,
-                                Some(value) => {
-                                    write_frame(stream, OwnedFrame::BulkString(value.into()))?
-                                }
-                            }
-                        }
-                        "set" => {
-                            assert!(values.len() == 3 || values.len() == 5);
-                            let key = string_from(1)?;
-                            let value = string_from(2)?;
-
-                            let expire_in = if values.len() == 5 {
-                                let px = string_from(3)?;
-                                assert_eq!(px.to_ascii_lowercase(), "px");
-                                let expire_in: u64 = string_from(4)?.parse()?;
-                                Some(Duration::from_millis(expire_in))
-                            } else {
-                                None
-                            };
-
-                            store.set(key, value, expire_in);
-                            write_frame(stream, OwnedFrame::SimpleString("OK".into()))?
-                        }
-                        command => panic!("unknown command: {}", command),
-                    }
-                }
-                _ => todo!(),
-            }
-        }
-        redis::Value::Status(_) => todo!(),
-        redis::Value::Okay => todo!(),
-    };
-
-    Ok(())
 }
---
+++ src/server.rs
@@ -0,0 +1,118 @@
+use redis::{FromRedisValue, RedisResult, Value};
+use redis_protocol::resp2::{encode::encode, types::OwnedFrame, types::Resp2Frame};
+use std::{
+    io::Write,
+    net::TcpStream,
+    sync::{Arc, Mutex},
+    time::Duration,
+};
+
+use anyhow::Result;
+
+use crate::store::Store;
+
+pub struct Server {
+    store: Arc<Mutex<Store>>,
+}
+
+fn write_frame(stream: &mut TcpStream, frame: OwnedFrame) -> Result<()> {
+    let mut buf = vec![0; frame.encode_len()];
+    encode(&mut buf, &frame)?;
+    stream.write_all(&buf)?;
+    Ok(())
+}
+
+impl Server {
+    pub fn new() -> Self {
+        Self {
+            store: Arc::new(Mutex::new(Store::new())),
+        }
+    }
+
+    pub fn handle_new_client(&self, mut stream: TcpStream) -> Result<()> {
+        let mut parser = redis::Parser::new();
+        loop {
+            let result = parser.parse_value(&stream);
+
+            match result {
+                Ok(value) => self.handle_redis_value(&mut stream, value)?,
+                Err(error) => {
+                    println!("Error: {:?}, will close connection", error.category());
+                    break;
+                }
+            }
+        }
+
+        Ok(())
+    }
+
+    fn handle_redis_value(&self, stream: &mut TcpStream, value: Value) -> Result<()> {
+        match value {
+            redis::Value::Nil => todo!(),
+            redis::Value::Int(_) => todo!(),
+            redis::Value::Data(data) => {
+                println!("Data: {:?}", data);
+            }
+            redis::Value::Bulk(values) => {
+                println!("Bulk: {:?}", values);
+
+                let string_from = |idx: usize| -> RedisResult<String> {
+                    String::from_owned_redis_value(values.get(idx).unwrap().clone())
+                };
+
+                match &values[0] {
+                    redis::Value::Data(command) => {
+                        match String::from_utf8_lossy(command)
+                            .to_ascii_lowercase()
+                            .as_str()
+                        {
+                            "ping" => write_frame(stream, OwnedFrame::BulkString("PONG".into()))?,
+                            "echo" => {
+                                assert_eq!(values.len(), 2);
+                                let string = string_from(1)?;
+                                write_frame(stream, OwnedFrame::BulkString(string.into()))?
+                            }
+                            "get" => {
+                                let store = self.store.lock().unwrap();
+
+                                assert_eq!(values.len(), 2);
+                                let key = string_from(1)?;
+                                match store.get(&key) {
+                                    None => write_frame(stream, OwnedFrame::Null)?,
+                                    Some(value) => {
+                                        write_frame(stream, OwnedFrame::BulkString(value.into()))?
+                                    }
+                                }
+                            }
+                            "set" => {
+                                let store = self.store.lock().unwrap();
+
+                                assert!(values.len() == 3 || values.len() == 5);
+                                let key = string_from(1)?;
+                                let value = string_from(2)?;
+
+                                let expire_in = if values.len() == 5 {
+                                    let px = string_from(3)?;
+                                    assert_eq!(px.to_ascii_lowercase(), "px");
+                                    let expire_in: u64 = string_from(4)?.parse()?;
+                                    Some(Duration::from_millis(expire_in))
+                                } else {
+                                    None
+                                };
+
+                                store.set(key, value, expire_in);
+                                write_frame(stream, OwnedFrame::SimpleString("OK".into()))?
+                            }
+                            command => panic!("unknown command: {}", command),
+                        }
+                    }
+                    _ => todo!(),
+                }
+            }
+            redis::Value::Status(_) => todo!(),
+            redis::Value::Okay => todo!(),
+        };
+
+        Ok(())
+    }
+}
