33ead406959b431059df04453f9dd9c281b2042c
Yue Yin <yueyin.dev@gmail.com>
2024-04-07 21:41:36 -0400

    port

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,34 @@
 mod server;
 mod store;
+use clap::Parser;
+use std::{
+    net::{Ipv4Addr, SocketAddrV4, TcpListener},
+    sync::Arc,
+    thread,
+};
 
-use std::{net::TcpListener, sync::Arc, thread};
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct Cli {
+    #[arg(long)]
+    port: Option<String>,
+}
 
 fn main() {
-    let server = Arc::new(server::Server::new());
+    let cli = Cli::parse();
 
-    let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
+    let port = match cli.port {
+        None => 6379,
+        Some(port) => {
+            let port: u16 = port.parse().unwrap();
+            port
+        }
+    };
+    let sockaddr = SocketAddrV4::new(Ipv4Addr::new(127, 0, 0, 1), port);
+    println!("Addr: {}", sockaddr);
+
+    let server = Arc::new(server::Server::new());
+    let listener = TcpListener::bind(sockaddr).unwrap();
     for stream in listener.incoming() {
         match stream {
             Ok(stream) => {
